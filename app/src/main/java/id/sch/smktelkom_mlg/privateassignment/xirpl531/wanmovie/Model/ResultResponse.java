d749ca84af09bef6cc7400ba21360724acba01d9
setiawan1999 <setiawan1999.sw@gmail.com>
2017-06-08 16:02:36 +0700

    Done

---
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl531/wanmovie/Model/ResultResponse.java
@@ -0,0 +1,8 @@
+package id.sch.smktelkom_mlg.privateassignment.xirpl531.wanmovie.Model;
+
+import java.util.List;
+
+public class ResultResponse {
+    public int page;
+    public List<Result> results;
+}
